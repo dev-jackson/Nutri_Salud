8da64356427daa6020de1079aa697dea4475c1df
Jackson Sanchez <angeljackson0933@gmail.com>
2020-02-04 22:50:05 -0500

    Arreglo Header

--- view/plantillas/header.php
+++ view/plantillas/header.php
@@ -6,17 +6,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- Bootstrap CSS -->
         <!---<link rel="stylesheet" href="assets/css/bootstrap.min.css" >--->
-        <link rel="stylesheet" href="../assets/css/inicio.css">
+        <link rel="stylesheet" href="./assets/css/inicio.css">
         <title>Nutricionista</title>
     </head>
     <body>
     <header id="header">
-    <a href="#" class="icon"><img src="../assets/imagenes/logo.png" alt="" width="45px"></a>
+    <a href="#" class="icon"><img src="./assets/imagenes/logo.png" alt="" width="45px"></a>
     <ul>
-        <li><a href="../view/inicio.php" class="active">INICIO</a></li>
-        <li><a href="../view/planes.php">PLANES</a></li>
+        <li><a href="./view/inicio.php" class="active">INICIO</a></li>
+        <li><a href="./view/planes.php">PLANES</a></li>
         <li><a href="#">INFROMACION</a></li>
-        <li><a href="../view/login.php">LOGIN</a></li>
+        <li><a href="./view/login.php">LOGIN</a></li>
     </ul>
     <div class="respon" onclick="toogle()"><img src="assets/imagenes/logo.png" alt="" width="30px"></div>
         </header>
