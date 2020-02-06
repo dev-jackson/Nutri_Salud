c3592568c4ecc753d3fdbfd59d466dac128225f1
kevinBry <kbryanp@hotmail.com>
2020-02-06 00:07:25 -0500

    pagina Nosotros.php

--- view/estaticas/nosotros.php
+++ view/estaticas/nosotros.php
@@ -1,3 +1,41 @@
-<div><p>nosotros</p>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="./assets/css/inicio.css">
+
+        <title>Nosotros</title>
+    </head>
+    <body>
+        <div style="background-image: url('./assets/imagenes/nosotros/imagen_fondoN.jpeg'); opacity: 0.8;">
+            <section id="inicio">
+                <div >
+                    <h2>NOSOTROS</h2>
+                    </br>
+                    <p>Todos tenemos derecho a sentirnos contentos en nuestro cuerpo, y disfrutar del bienestar 
+                        que redunda de un estilo de vida saludable. Todos podemos, hay que querer hacerlo.</p>
+                    </br>
+                    <h3>Marianela Fernandes</h3>
+                    <ul style="padding: 4%">
+                        <li>Dietista-Nutricionista</li>
+                        <li>Diplomada y Graduada en Nutrición Humana y Dietética.</li>
+                        <li>Máster Oficial de Nutrición en la Actividad Física y el Deporte</li>
+                        <li>Máster Oficial de Nutrición en la Actividad Física y el Deporte</li>
+                    </ul>
+                </div>
+                <!-- aqui inserta una imagen de unos doctores bien mamados :V y en los otros temas distintos de lo que hay en el inicio -->
+                <img src="./assets/imagenes/nosotros/inicioNosotros.png" alt="" >
+            </section>
+        </div>
+        
+        <script type="text/javascript">
+            function toogle() {
+                var header = document.getElementById("header")
+                header.classList.toggle('active')
+            }
+        </script>        
+    </body>
+</html>
 
