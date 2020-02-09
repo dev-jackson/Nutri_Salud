840639ece53285997b61af43a8797022ad4e2273
kevinBry <kbryanp@hotmail.com>
2020-02-09 17:20:58 -0500

    pagina nosotros.php

--- view/estaticas/nosotros.php
+++ view/estaticas/nosotros.php
@@ -1,28 +1,29 @@
-        <div style="background-image: url('./assets/imagenes/nosotros/imagen_fondoN.jpeg'); opacity: 0.8;">
-            <section id="inicio">
-                <div >
-                    <h2>NOSOTROS</h2>
-                    </br>
-                    <p>Todos tenemos derecho a sentirnos contentos en nuestro cuerpo, y disfrutar del bienestar 
-                        que redunda de un estilo de vida saludable. Todos podemos, hay que querer hacerlo.</p>
-                    </br>
-                    <h3>Marianela Fernandes</h3>
-                    <ul style="padding: 4%">
-                        <li>Dietista-Nutricionista</li>
-                        <li>Diplomada y Graduada en Nutrición Humana y Dietética.</li>
-                        <li>Máster Oficial de Nutrición en la Actividad Física y el Deporte</li>
-                        <li>Máster Oficial de Nutrición en la Actividad Física y el Deporte</li>
-                    </ul>
-                </div>
-                <!-- aqui inserta una imagen de unos doctores bien mamados :V y en los otros temas distintos de lo que hay en el inicio -->
-                <img src="./assets/imagenes/nosotros/inicioNosotros.png" alt="" >
-            </section>
+<!--<div style="background-image: url('./assets/imagenes/nosotros/imagen_fondoN.jpeg'); opacity: 0.8;">-->
+<div style="background-color: #1c7430">
+    <section id="inicio">
+        <div>
+            <h2>NOSOTROS</h2>
+            </br>
+            <p>Todos tenemos derecho a sentirnos contentos en nuestro cuerpo, y disfrutar del bienestar 
+                que redunda de un estilo de vida saludable. Todos podemos, hay que querer hacerlo.</p>
+            </br>
+            <h3>Marianela Fernandes</h3>
+            <ul style="padding: 4%">
+                <li>Dietista-Nutricionista</li>
+                <li>Diplomada y Graduada en Nutrición Humana y Dietética.</li>
+                <li>Máster Oficial de Nutrición en la Actividad Física y el Deporte</li>
+                <li>Máster Oficial de Nutrición en la Actividad Física y el Deporte</li>
+            </ul>
         </div>
-        
-        <script type="text/javascript">
-            function toogle() {
-                var header = document.getElementById("header")
-                header.classList.toggle('active')
-            }
-        </script>        
+        <!-- aqui inserta una imagen de unos doctores bien mamados :V y en los otros temas distintos de lo que hay en el inicio -->
+        <img src="./assets/imagenes/nosotros/nosotros.png" alt="" >
+    </section>
+</div>
+
+<script type="text/javascript">
+    function toogle() {
+        var header = document.getElementById("header")
+        header.classList.toggle('active')
+    }
+</script>        
 
