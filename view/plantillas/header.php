b6d4fafc83c1836a7a99a6e0ad94c40fcf3d36aa
Jackson Sanchez <angeljackson0933@gmail.com>
2020-02-09 20:23:34 -0500

    Add Alert

--- view/plantillas/header.php
+++ view/plantillas/header.php
@@ -7,6 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- Bootstrap CSS -->
         <!---<link rel="stylesheet" href="./assets/css/bootstrap.min.css" >-->
+        <script src="./assets/js/sweetalert.min.js"></script>
         <link rel="stylesheet" href="./assets/css/inicio.css">
         <link rel="stylesheet" href="./assets/css/index.css">
     
@@ -19,7 +20,13 @@
         <li><a href="index.php?" class="active" id="inicio">INICIO</a></li>
         <li><a href="index.php?c=index&c=Cliente&a=verPlanes" id="planes">PLANES</a></li>
         <li><a href="index.php?c=index&a=estatica&p=nosotros" id="nosotros">NOSOTROS</a></li>
-        <li><a href="index.php?c=index&a=login"><?php if(isset($_SESSION['Adm'])){
+        <li><a  <?php
+            if(isset($_SESSION['Amd'])||isset($_SESSION['Client'])){
+                echo "href='#' onclick='cerrar();'";
+            }else{
+                echo "href='index.php?c=index&a=login'";
+            }
+        ?>><?php if(isset($_SESSION['Adm'])){
             echo strtoupper ($_SESSION['Adm'])."(ADM)".'<a>salir</a>';   
         }elseif(isset($_SESSION['Client'])){
             echo strtoupper ($_SESSION['Client']);
@@ -30,6 +37,36 @@
     </ul>
     <div class="respon" onclick="toogle()"><img src="assets/imagenes/logo.png" alt="" width="30px"></div>
         </header>
+    <script type="text/javascript">
+        function cerrar(){
+            swal({
+                     closeOnClickOutside:false,
+                     title: "Aviso !",
+                     text: "Esta seguro de cerrar Sesion",
+                     icon: "warning",
+                     buttons: {
+                     si:{ 
+                      text:"si",
+                      value:"si"
+                      },
+                      no:{ 
+                      text:"no",
+                      value:"no"
+                      },
+                      },
+                })
+                .then((value) => {
+                switch (value) {                                     
+                case "si":
+                           window.location.href ="login.php";    
+                  break;
+                case "no":
+                               
+                  break;
+            }
+          })
+        }
+    </script>
     </body>
 </html>
 </div>
